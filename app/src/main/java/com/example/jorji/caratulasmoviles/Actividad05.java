02c02953403443ca6cb965284f66ca4ea64808b9
JordanJL <jorjimlop@outlook.com>
2018-05-15 12:03:27 -0600

    new

---
+++ app/src/main/java/com/example/jorji/caratulasmoviles/Actividad05.java
@@ -0,0 +1,13 @@
+package com.example.jorji.caratulasmoviles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividad05 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad05);
+    }
+}
